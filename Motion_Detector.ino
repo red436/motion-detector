fdc6d6be693d4fd8606fc791b01ca5f8633a42e7
red436 <redbeard.436@gmail.com>
2022-07-26 15:34:43 -0700

    Update Motion_Detector.ino

--- Motion_Detector.ino
+++ Motion_Detector.ino
@@ -8,25 +8,25 @@
 WiFiMulti wifiMulti;
 HTTPClient http;
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton
+const int PIRPin = 2;     // the number of the PIR motion sensor
 const int ledPin = 13;
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int PIRState = 0;         // variable for reading the PIR motion status
 
 void setup() {
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(PIRPin, INPUT);
   pinMode(ledPin, OUTPUT);
-  wifiMulti.addAP("Cabezud", "7tWRLmeYWJA");
+  wifiMulti.addAP("AP_SSID", "AP_PASS");
 }
 
 void loop() {
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+  PIRState = digitalRead(PIRPin);
   http.begin("https://api.pushbullet.com/v2/pushes");
   http.addHeader("Content-Type", "application/json");
-  http.addHeader("Access-Token", "o.vLD8TMCvkLFg7M4qxWRhqzSZA8XpNssg");
+  http.addHeader("Access-Token", "");
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
